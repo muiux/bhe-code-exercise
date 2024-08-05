ff6e821f15f2511d3e24ebb224813fdd6e73f0ee
Ronnie Sarris <ronniesarris11@gmail.com>
2024-08-04 22:51:05 -0400

    NthPrime algorithm in javascript

--- javascript/Sieve/sieve.js
+++ javascript/Sieve/sieve.js
@@ -1,6 +1,31 @@
 class Sieve {
   NthPrime(n) {
-    throw new Error("unimplemented");
+    if (n === 0) return 2;
+
+    let count = 1;
+    let num = 1;
+    const primes = [2];
+
+    while (count <= n) {
+      num += 2;
+      let isPrime = true;
+      const sqrt = Math.sqrt(num);
+
+      for (const prime of primes) {
+        if (prime > sqrt) break;
+        if (num % prime === 0) {
+          isPrime = false;
+          break;
+        }
+      }
+
+      if (isPrime) {
+        primes.push(num);
+        count++;
+      }
+    }
+
+    return primes[n];
   }
 }
 
